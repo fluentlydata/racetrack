ea9374046dfb490087a36f2858c0558b5b7d4fb9
Patrick Buchter <patrick.buchter@jaywalker-digital.ch>
2017-02-18 16:18:51 +0100

    added websocket example

--- server/src/main/scala/ScalatraBootstrap.scala
+++ server/src/main/scala/ScalatraBootstrap.scala
@@ -5,5 +5,6 @@ import javax.servlet.ServletContext
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     context.mount(new RacetrackServlet, "/*")
+    context.mount(new WebsocketController, "/ws")
   }
 }
