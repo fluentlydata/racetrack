704eeda620608f10faed1ac9ba78c3204c49937d
Patrick Buchter <patrick.buchter@jaywalker-digital.ch>
2017-03-02 19:24:22 +0100

    added jquery when/done with deferred objects

--- server2/public/javascripts/game.js
+++ server2/public/javascripts/game.js
@@ -1,7 +1,16 @@
 // global variable (todo: change)
 var blockSize = 0
-var cx = -1
-var cy = -1
+var canvas;
+var canvasHeight, canvasWidth;
+var trackWidth, trackHeight;
+var ctx;
+
+var trackId = 0;
+var token   = "test";
+var track = [];
+
+var carx = -1;
+var cary = -1;
 
 function connectToWS(trackId, token) {
     var ws = new WebSocket("ws://localhost:9000/register");
@@ -11,22 +20,20 @@ function connectToWS(trackId, token) {
     };
 
     ws.onmessage = function(event) {
-
         $.get("/car/test", function(c) {
             var x = c["px"]
             var y = c["py"]
 
             // only update if car pos changed
-            if (cx != x | cy != y) {
+            if (carx !== x || cary !== y) {
 
                 // redraw everything with new posiiton
-                drawBackground(trackId);
-                for (var i=0;i<10000;i++)
-                drawCar(token);
+                drawBackground();
+                drawCar(c);
 
                 // update current coordinates
-                cx = x;
-                cy = y;
+                carx = x;
+                cary = y;
             }
         });
     }
@@ -34,6 +41,22 @@ function connectToWS(trackId, token) {
     ws.onerror = function(evt) {
         console.log("Error!");
     };
+}
+
+function setGlobalTrackWidthHeight(jsonTrack) {
+    // first find out the size of the field
+    var height = 0;
+    var width = 0;
+    jsonTrack["fields"].forEach(function(f) {
+        var x = f["x"];
+        var y = f["y"];
+        if (x>width) width = x
+        if (y>height) height = y
+    });
+
+    // highest value + 1 => dimension
+    trackHeight = height + 1;
+    trackWidth  = width + 1;
 }
 
 /*
@@ -45,24 +68,10 @@ function connectToWS(trackId, token) {
 function convertToArray(json) {
   var a = [];
 
-  // first find out the size of the field
-  var height = 0;
-  var width = 0;
-  json["fields"].forEach(function(f) {
-    var x = f["x"];
-    var y = f["y"];
-    if (x>width) width = x
-    if (y>height) height = y
-  });
-
-  // highest value + 1 => dimension
-  height = height + 1;
-  width = width + 1;
-
   // initialize a with zeros
-  for (row=0;row<height;row++) {
-    a.push(new Array(width));
-    for (col=0;col<width;col++) {
+  for (row = 0; row < trackHeight; row++) {
+    a.push(new Array(trackWidth));
+    for (col = 0; col < trackWidth; col++) {
       a[row][col] = 0;
     }
   }
@@ -78,37 +87,17 @@ function convertToArray(json) {
   return a;
 }
 
-function setBlockSize(id) {
-    var canvas = document.getElementById("myCanvas");
-    var cw = canvas.width;
-    var ch = canvas.height;
-    var ctx = canvas.getContext("2d");
-
-    // get track as json
-    $.get("/track/" + id, function(t) {
-        track = convertToArray(t);
-        var height = track.length
-        var width  = track[0].length
-        var blockWidth = cw/width;
-        var blockHeight = cw/height;
-        blockSize = blockWidth < blockHeight ? blockWidth : blockHeight;
-
-        console.log("track height: " + height)
-        console.log("track width: "  + width)
-        console.log("block size: "   + blockSize)
-    });
+function setGlobalBlockSize() {
+    var blockWidth = canvasWidth/trackWidth;
+    var blockHeight = canvasHeight/trackHeight;
+    blockSize = blockWidth < blockHeight ? blockWidth : blockHeight;
 }
+
 
 /*
  * draws track (static background) on canvas
  */
-function drawBackgroundCanvas(track) {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-
-    var height = track.length
-    var width  = track[0].length
-
+function drawBackground() {
     var styleMap = {
         0: '#e5e5e5', // race floor
         1: '#c6c6c6', // border/wall
@@ -122,60 +111,75 @@ function drawBackgroundCanvas(track) {
           ctx.strokeRect(x*blockSize, y*blockSize, blockSize, blockSize);
     }
 
-    for(var row=0; row < height; row++) {
-      for(var col=0; col < width; col++) {
+    for(var row=0; row < trackHeight; row++) {
+      for(var col=0; col < trackWidth; col++) {
         draw(row, col, track[row][col]);
       }
     }
 }
 
-function drawCar(token) {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-
-    function draw(x,y) {
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
-    }
-
-    $.get("/car/" + token, function(c) {
-        var x = c["px"]
-        var y = c["py"]
-        console.log("draw car at pos (x|y): (" + x + "|" + y + ")")
-        draw(x,y);
-    });
+function drawCar(car) {
+    var x = car["px"]
+    var y = car["py"]
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
 }
 
-function drawBackground(id) {
-    $.get("/track/" + id, function(t) {
-        track = convertToArray(t);
-        drawBackgroundCanvas(track);
-    });
-}
 
-function moveTestCar() {
-    // Post to /move/test with jquery
-    $.get("/car/test", function(c) {
-        var x = c["px"]
-        var y = c["py"]
-        $.post("/move/test", {"px": x+1, "py": y})
-    });
+function moveTestCarForward() {
+     // Post to /move/test with jquery
+     $.get("/car/test", function(c) {
+         var x = c["px"]
+         var y = c["py"]
+         $.post("/move/test", {"px": x+1, "py": y})
+     });
 }
 
 function initialize() {
 
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+    canvasHeight = canvas.height;
+    canvasWidth = canvas.width;
+
     // todo: testing...
-    var trackId = 0;
-    var token   = "test";
+    trackId = 0;
+    token   = "test";
 
     connectToWS(trackId, token);
 
-    setBlockSize(trackId);
-    for (var i=0;i<10000;i++)
-    drawBackground(trackId);
-    for (var i=0;i<10000;i++)
-    drawCar(token);
-}
+    // async tasks
+    var deferredTrack = $.get("/track/0");
+    var deferredCar   = $.get("/car/test");
+
+    $.when( deferredTrack, deferredCar )
+     .done(
+         // called, if all async tasks are done
+         function(t, c) {
+
+             // get returns an array where the first element is the data returned by the server
+             var jsonTrack = t[0];
+             var jsonCar   = c[0];
+
+             console.log(jsonTrack)
+             console.log(jsonCar)
+
+             // this sets trackHeight, trackWidth
+             setGlobalTrackWidthHeight(jsonTrack);
+
+             // track is a 2d-array. also a global variable..
+             track = convertToArray(jsonTrack);
+             console.log(track)
+
+             setGlobalBlockSize();
+             console.log(blockSize)
+
+             drawBackground();
+
+             drawCar(jsonCar);
+         }
+     )
+ }
 
 window.onload = initialize
 
